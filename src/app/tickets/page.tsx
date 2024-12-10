67b9db814264e95c7de5c8c3a8d6432ac9b96d51
DevApe <khidash8@gmail.com>
2024-12-10 08:06:55 +0530

    clsx npm package to add conditional styling

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from 'react';
 import {initialTickets} from "@/tickets";
 import {ticketPath} from "@/utilities/path";
+import clsx from "clsx";
 
 const ticketIcons ={
     open: 'O',
@@ -15,8 +16,10 @@ const TicketsPage = () => {
         <div className={'flex flex-col gap-2 items-center justify-center'}>
             <h1 className={'text-2xl'}>All the tickets</h1>
             {initialTickets.map(ticket => (
-                <div key={ticket.id} className={'border p-2 w-full mx-3'}>
-                    <h2 className={'text-lg'}>{ticket.title}</h2>
+                <div key={ticket.id} className={'border p-2 w-full mx-3 rounded-lg'}>
+                    <h2 className={clsx('text-lg',{
+                        'line-through': ticket.status === 'closed'
+                    })}>{ticket.title}</h2>
                     <p>{ticket.description}</p>
                     <p>{ticketIcons[ticket.status]}</p>
                     <Link href={ticketPath(ticket.id)} className={'underline w-full flex justify-center'}>View
