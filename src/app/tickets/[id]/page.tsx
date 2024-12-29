5969323d2b26d051a67bcdba45b35536f4e03320
DevApe <khidash8@gmail.com>
2024-12-29 20:34:57 +0530

    type check param.id fix

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -2,24 +2,22 @@ import {notFound} from "next/navigation";
 import TicketItem from "@/features/ticket/components/ticketItem";
 import {getTicket} from "@/features/ticket/queries/get-ticket";
 
-interface TicketProps {
-    params: {
-        id: string;
-    };
-}
-
-const TicketPage = async ({params}: TicketProps) => {
-    const ticket = await getTicket(params.id)
+const TicketPage = async ({params}: { params: Promise<{ ticketId: string }>}) =>
+{
+    const { ticketId } = await params;
+    const ticket = await getTicket(ticketId);
 
     if (!ticket) {
         notFound()
     }
 
     return (
-        <div className={'flex-1 flex flex-col gap-y-8 items-center self-center w-full max-w-[420px] animate-fade-in-from-top'}>
+        <div
+            className={'flex-1 flex flex-col gap-y-8 items-center self-center w-full max-w-[420px] animate-fade-in-from-top'}>
             <TicketItem ticket={ticket} isDetail/>
         </div>
     );
-};
+}
+;
 
 export default TicketPage;
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,6 +1,7 @@
+import {Ticket} from "@prisma/client";
 import {prisma} from "@/lib/prisma";
 
-export const getTicket = async (ticketId: string) => {
+export const getTicket = async (ticketId: string): Promise<Ticket | null>  => {
     return prisma.ticket.findUnique({
         where: {
             id: ticketId
